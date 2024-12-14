c5a922860253dda768d567cf5a3a8de6f024106d
boris <boriskozinn@gmail.com>
2024-12-14 23:19:26 +0300

    update code

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from body import Body
 from Config import *
 import numpy as np
-
-
-# import cupy as cp
+import cupy as cp
 
 def get_force(body1: Body, body2: Body) -> np.ndarray:
     v: np.ndarray = np.array([body2.x - body1.x, body2.y - body1.y])
-    return G * body1.mass * body2.mass / np.linalg.norm(v) ** 3 * v
+    len_v: float = norm if (norm := np.linalg.norm(v)) >= EPS else EPS
+
+    return G * body1.mass * body2.mass / len_v ** 3 * v
 
 
 def calc_f(bodies: list[Body]) -> list[np.ndarray]:
     f_mat: list[list[np.ndarray]] = [[np.array([0, 0])] * len(bodies) for _ in range(len(bodies))]
+
     for ind1, body1 in enumerate(bodies[1:]):
         for ind2, body2 in enumerate(bodies[:ind1 + 1]):
             f_mat[ind1 + 1][ind2] = get_force(body1, body2)
@@ -23,22 +24,30 @@ def calc_f(bodies: list[Body]) -> list[np.ndarray]:
 def task1_run(bodies: list[Body], t_end: float, dt: float) -> list[list[float]]:
     result: list[list[float]] = []
     local_result: list[float] = [0]
+
     for body in bodies:
         local_result.append(body.x)
         local_result.append(body.y)
+
     result.append(local_result)
     it: int = int(np.ceil(t_end / dt))
+
     for i in range(it):
         forces: list[np.ndarray] = calc_f(bodies)
         local_result: list[float] = [dt * (i + 1)]
+
         for ind, body in enumerate(bodies):
             body.x += body.vx * dt
             body.y += body.vy * dt
+
             body.vx += forces[ind][0] / body.mass * dt
             body.vy += forces[ind][1] / body.mass * dt
+
             local_result.append(body.x)
             local_result.append(body.y)
+
         result.append(local_result)
+
     return result
 
 
--- plot.py
+++ plot.py
@@ -1,6 +1,9 @@
-import numpy as np
+import multiprocessing
+import os
 import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib.animation import FuncAnimation
+from multiprocessing import Pool
 from Config import *
 
 result = []
@@ -16,15 +19,48 @@ ax.set_xlim(LEFT_EDGE, RIGHT_EDGE)
 ax.set_ylim(LEFT_EDGE, RIGHT_EDGE)
 
 
-# Функция для обновления данных на каждом кадре
 def update(frame):
     x = result[frame][::2]
     y = result[frame][1::2]
-
     scat.set_offsets(np.c_[x, y])
     return scat,
 
 
-ani = FuncAnimation(fig, update, frames=len(result), interval=TIME_DELAY, blit=True)
+def process_frames(start, end):
+    ani = FuncAnimation(fig, update, frames=range(start, end), interval=1000 / FPS, blit=True)
+    ani.save(f"part_{start}_{end}.mp4", writer="ffmpeg", fps=FPS)
 
-ani.save("animation.mp4", writer="ffmpeg", fps=1000 // TIME_DELAY)
+
+def main():
+    num_processes = multiprocessing.cpu_count()
+
+    frames_per_process = len(result) // num_processes
+    remaining_frames = len(result) % num_processes
+
+    ranges = []
+    for i in range(num_processes):
+        start = i * frames_per_process
+        end = (i + 1) * frames_per_process + (remaining_frames if i == num_processes - 1 else 0)
+        ranges.append((start, end))
+
+    with Pool(processes=num_processes) as pool:
+        pool.starmap(process_frames, ranges)
+
+    pool.close()
+    pool.join()
+
+    with open('file_list.txt', 'w') as f:
+        for i in range(num_processes):
+            f.write(f"file 'part_{ranges[i][0]}_{ranges[i][1]}.mp4'\n")
+
+    os.system("ffmpeg -f concat -safe 0 -i file_list.txt -c copy -y -loglevel quiet animation.mp4")
+    os.startfile("animation.mp4")
+
+    for i in range(num_processes):
+        os.remove(f"part_{ranges[i][0]}_{ranges[i][1]}.mp4")
+
+    os.remove('file_list.txt')
+
+
+if __name__ == '__main__':
+    main()
